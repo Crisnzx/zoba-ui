1db2ae019c12f6ce649f8f50153fe594ed18c207
Crisnzx <cristovaon13@gmail.com>
2023-01-04 00:32:47 -0300

    chore: Update rollup configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,23 +2,42 @@ import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/index.es.js",
-    format: "cjs",
-    exports: "named",
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: true,
+        exports: "named",
+      },
+      {
+        file: "dist/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+        exports: "named",
+      },
+    ],
+    plugins: [
+      // babel({
+      //   exclude: "node_modules/**",
+      //   presets: ["@babel/preset-react"],
+      // }),
+      // external(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      // terser(),
+    ],
   },
-  plugins: [
-    babel({
-      exclude: "node_modules/**",
-      presets: ["@babel/preset-react"],
-    }),
-    external(),
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-};
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
