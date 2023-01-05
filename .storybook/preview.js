e36bba33754ae9f4e732697467c2af60d4999900
Crisnzx <cristovaon13@gmail.com>
2023-01-04 21:21:49 -0300

    feat: Apply css only in development

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import "../src/styles/reset.scss";
+import "../src/components/alert/styles.scss";
+import "../src/components/form-components/switch/styles.scss";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # zoba-ui
+
+- A React component library that abstracts the implementations but at the same time gives you the power of styling the way you want
+
+- For the standard component styles to work properly, you should use our `reset.scss` file in your project.
