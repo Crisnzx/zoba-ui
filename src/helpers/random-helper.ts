badb09c195f25122152aadb5002d13bed3a3630c
Crisnzx <cristovaon13@gmail.com>
2023-01-07 00:24:22 -0300

    chore: Add random-helper

---
+++ src/helpers/random-helper.ts
@@ -0,0 +1,15 @@
+export class RandomHelper {
+  private constructor() {}
+
+  public static string(length = 5): string {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+}
